b0dcb3ec4092d92a5ec0de3da5772a5cc44709a7
James Montgomery <erenroseyeager@gmail.com>
2024-07-15 12:59:21 -0400

    - Deleted planet.js
    - Applied new features
    - Passed tests

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -6,11 +6,9 @@ const urlPlanets = 'http://localhost:9009/api/planets'
 const urlPeople = 'http://localhost:9009/api/people'
 
 function App() {
-  // ❗ Create state to hold the data from the API
   const [apod, setApod] = useState([])
   const [aplan, setAplan] = useState([])
   
-  // ❗ Create effects to fetch the data and put it in state
 useEffect(() => {
   axios.get(urlPlanets)
       .then(data => setAplan(data.data))
@@ -21,22 +19,21 @@ useEffect(() => {
 
 let planets = aplan
 let characters = apod
-//let homeworlds = homeworld
 
-for (let i = 0; i < characters.length; i++) {
-  characters[i].planet = planets[i].name
-  //homeworlds[i].show = false
-}
+ for (let i = 0; i < characters.length; i++) {
+    let characterhomeworldid = characters[i].homeworld
+    let foundplanet = planets.find(planet => planet.id === characterhomeworldid)
+    characters[i].planet = foundplanet.name
+ }
 
   return (
     <div>
       <h2>Star Wars Characters</h2>
       <p>See the README of the project for instructions on completing this challenge</p>
       {
-      // ❗ Map over the data in state, rendering a Character at each iteration 
         apod.map((list)=> (
-        <Character props={{ name: list.name, planet: list.planet, id: list.id, show: list.show }}/>
-       ))
+          <Character props={{ name: list.name, planet: list.planet, id: list.id, show: list.show }}/>
+         ))
       }
     </div>
   )
--- frontend/components/Character.js
+++ frontend/components/Character.js
@@ -1,58 +1,24 @@
 import React from 'react'
-import Planet from './Planet'
-//import styled from 'styled-components'
 import { useState } from 'react'
 
-function Character({ props }) { // ❗ Add the props
-  // ❗ Create a state to hold whether the homeworld is rendering or not
-  const [homeworld, setHomeworld] = useState([])
-  //setHomeworld(props.show)
-
-// ❗ Create a "toggle" click handler to show or remove the homeworld
-
-const clickMe = (propId) => () => {
-  const myElement = document.getElementById(propId)
-  if(myElement.style.display === "none"){
-    myElement.style.display = "block"
-  } else {
-    myElement.style.display = "none"
-  }
-}
-
-const handleclick = () => () => {
-  debugger
-  const myElement = document.getElementById(propId)
-  if(myElement.style.display === "none"){
-    myElement.style.display = "block"
-  } else {
-    myElement.style.display = "none"
-  }
-}
-
-
-  //if(props.show) {
+function Character({ props }) { 
+  const [homeworld, setHomeworld] = useState(false)
+    
     return (
-    <div onClick={(e) => clickMe(props.id)(e)} className="character-card">
+    <div onClick={() => setHomeworld(homeworld ? false : true)} className="character-card">
     <h3 className="character-name">
       {props.name}
     </h3>
-    <Planet props={{ name: props.name, planet: props.planet, id: props.id, show: props.show }} />
+      {homeworld ?
+      <p name={props.id} id={props.id}>
+            Planet: 
+            <span className="character-planet">
+              {props.planet}
+            </span>
+            </p>
+    : ""}
     </div>    
     )
-  // } else {
-  //   return (
-  //     <div onClick={(e) => clickMe(props.id)(e)} className="character-card">
-  //       <h3 className="character-name">
-  //         {props.name}
-  //       </h3>
-  //       <Planet props={{ name: props.name, planet: props.planet, id: props.id, show: props.show }} />
-  
-  //     </div>    
-  //   )
-  // }
-
 }
 
 export default Character
-
-
